e504658cc722fa03bcf0a11c00d6fd4b0ba46d38
seoilgun <seoilgun@gmail.com>
2022-04-27 14:49:13 +0900

    added timm models added custom dataset

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ parser.add_argument('--finetune-label-json', default=None, type=str, help='test 
 parser.add_argument('--test-label-json', default=None, type=str, help='test data path')
 parser.add_argument('--label-type', default=None, type=str, help='test data path')
 parser.add_argument('--save-path', default='./checkpoint', type=str, help='save path')
-parser.add_argument('--dataset', default='cifar10', type=str,
-                    choices=['cifar10', 'cifar100'], help='dataset name')
+parser.add_argument('--dataset', default='cifar10', type=str, help='dataset name')
 parser.add_argument('--num-labeled', type=int, default=4000, help='number of labeled data')
 parser.add_argument("--expand-labels", action="store_true", help="expand labels to fit eval steps")
 parser.add_argument('--total-steps', default=300000, type=int, help='number of total steps to run')
